41a11a3917c614b19bcb30a6d351784b2bfdc234
dyh_a <xinzon32177@163.com>
2022-05-13 23:42:45 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # use-axios
 使用插件的方式封装axios
+
+在request-template基础上，使用插件方式优化
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // 默认为node
+  // testEnvironment: 'jsdom',
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
